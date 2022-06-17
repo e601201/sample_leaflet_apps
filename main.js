3ba091870d9342d22ff3267da810ee8b80100292
e601201 <k.arthur1111@gmail.com>
2022-06-17 23:02:43 +0900

    [ADD]clickEvent

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ window.onload = (event)=>{
   console.log("onload!!");
 
   // 緯度経度データ
-  const Fukuoka = [33.590130104230596, 130.42041778564456];
+  const Fukuoka = [33.590346833436634, 130.41623353958133];
+  const Hakataeki = [33.587808609821906, 130.41222095489505];
   // マップのズーム度合い
   const ZoomLevel = 16;
   // マップを作る
@@ -21,13 +22,15 @@ window.onload = (event)=>{
 
   // マーカーを出す
   L.marker(Fukuoka).addTo(map)
-  .bindPopup("welcome to fukuoka!!").openPopup();
+  .bindPopup("here is fukuoka!!").openPopup();
 
   // 円を描く
-  let circle = L.circle(Fukuoka, {
+  let circle = L.circle(Hakataeki, {
+    //属性(縁の色、中の色、透明度、円の半径)
     color: "red", fillColor: "#f03",
-    fillOpacity: 0.2, radius: 100
+    fillOpacity: 0.4, radius: 100
   }).addTo(map);
+  circle.bindPopup("I am a circle.");
 
   //四角で囲む
   let polygon = L.polygon([
@@ -36,7 +39,7 @@ window.onload = (event)=>{
     [33.58721873026, 130.42412996292117],
     [33.592116395859996, 130.4240441322327]
   ]).addTo(map);
-
+  polygon.bindPopup("I am a polygon.");
   // クリックイベント
   let popup = L.popup();
   map.on("click", (e)=>{
@@ -44,4 +47,8 @@ window.onload = (event)=>{
     let str = e.latlng.lat + ", " + e.latlng.lng;
     popup.setLatLng(e.latlng).setContent(str).openOn(map);
   });
+  function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+    map.on('click', onMapClick);
+  }
 }
